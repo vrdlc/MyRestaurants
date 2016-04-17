42aa53858053a31dbc46936587855407fa293091
vrdlc <virgilioramon.delacruz@gmail.com>
2016-04-17 07:45:58 -0700

    do not understand enough to properly test

---
+++ app/src/test/java/com/example/ramon/myrestaurants/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.ramon.myrestaurants;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Ramon on 4/17/16.
+ */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+}
+
+
